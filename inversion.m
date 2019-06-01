dcdb6adbabac6d5640c06ee101c15c542bc099c7
Dr. Kaputa <dskiee@rit.edu>
2019-06-01 13:34:11 -0400

    was able to get the 45 deg roll rotation to work with Peter's great help!!

--- inversion.m
+++ inversion.m
@@ -1,8 +1,43 @@
-% perform inversion to bound an april tag given a pose
+% image inversion routine
 
-Tapril = SE3(0,0,1);
-corners = [.1 -.1 -.1 .1;
-           .1 .1 -.1 -.1;
-            0  0   0   0];       
+% camera assumptions
+% z = forward
+% x = right
+% y = down
+
+% drone and camera reference frame
+% x = forward [optical direction]
+% y = right
+% z = down
+
+% need to rotate by -90 in x and then -90 in z to get to NED
+
+% translation in meters and rotation in degrees
+% x,y,z roll, pitch, yaw [NED reference frame]
+pose = [-1 0 0 45 0 0];
+
+% create object with a given pose 
+Tapril = SE3(0,0,0);
+Tquad = SE3(pose(1:3)) * SE3.rpy(pose(4:6),'deg', 'zyx');
+Tcamera = SE3.Ry(90,'deg') * SE3.Rz(90,'deg');
+
+% set the corners for the object with the pose listed above
+corners = [ 0  0   0   0;
+           .1 -.1 -.1 .1;
+           .1 .1 -.1 -.1;] .* .1;       
+        
+% create default camera
 cam = CentralCamera('default');
-p = cam.project(corners,'objpose',Tapril)
+
+% project the corners onto the camera focal plane
+p = cam.project(corners,'objpose',Tapril, 'pose', Tquad*Tcamera)
+
+figure
+scatter(p(1,:),p(2,:));
+figure
+plot(Tapril,'color','r')
+hold on
+plot(Tquad * Tcamera,'color','b')
+%plot(Tquad,'color','g')
+set(gca, 'Zdir', 'reverse')
+set(gca, 'Ydir', 'reverse')
